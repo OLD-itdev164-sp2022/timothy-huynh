58224098d39bfb6c9f1f4fdc49300f83dad4ed88
Tim <huynht5@gmatc.matc.edu>
2022-02-17 21:00:54 -0600

    Secured Contentful Keys

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog`,
@@ -15,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `kp9qdcouj78j`,
-        accessToken: `u6q9tBC9T4WpwsXztUORocME6Gujymy8JqyUN_eqkYQ`,
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`,
       },
     },
     `gatsby-transformer-sharp`,
