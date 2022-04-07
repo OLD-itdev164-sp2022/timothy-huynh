98178a0eb6f56bac626adb9a53f76a6e31312abe
TimothyHuynh212 <skyborn225@gmail.com>
2022-04-07 00:12:55 -0500

    Updated the Header component

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import styled from "styled-components"
-import { Search } from "styled-icons/feather"
+import styled, { ThemeConsumer } from "styled-components"
+import { Flex } from "rebass"
 import { H1 } from "../Heading"
-import { IconButton } from "../Button"
+import { SearchButton } from "../Button"
 import { Section } from "../Section"
 
 const Outer = styled.header`
@@ -25,8 +25,25 @@ const Inner = styled.div`
 const StyledLink = styled(Link)`
   color: white;
   text-decoration: none;
-  &:hover {
-    color: purple;
+  margin: 0 10px;
+`
+
+const Image = styled.img`
+  margin: 0;`
+
+const Nav = styled(Flex)`
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const Title = styled(H1)`
+  flex: 4;
+`
+
+const MediaQuery = styled.div`
+  @media (max-width: 450px) {
+    display: none;
   }
 `
 
@@ -34,15 +51,25 @@ const Header = ({ siteTitle }) => (
   <Outer>
     <Inner>
       <Section flex>
-        <Section width={11/12}>
-          <H1>
-            <StyledLink to="/">
-              {siteTitle}
-            </StyledLink>
-          </H1>
-        </Section>
-        <Section width={1/12}>
-          <IconButton icon={<Search />} variant='contrast' />
+        <Section width={1/12}
+          flex flexDirection="column" justifyContent="center">
+            <ThemeConsumer>
+              {theme => <Image src={theme.images.mainHeaderImage} />}
+            </ThemeConsumer>
+        </Section>    
+        <Section width={11/12}
+          flex flexDirection= "column" justifyContent="center">
+          <Nav>
+            <Title>
+              <StyledLink to="/">{siteTitle}</StyledLink>
+            </Title>
+            <MediaQuery>
+              <StyledLink to="/">Home</StyledLink>
+              <StyledLink to="/about">About</StyledLink>
+              <StyledLink to="/contact">Contact</StyledLink>
+            </MediaQuery>
+            <SearchButton variant="contrast" />
+          </Nav>
         </Section>
       </Section>
     </Inner>
